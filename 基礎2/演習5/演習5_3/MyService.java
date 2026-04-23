26cdb8a07af48c0fbd71bc45ab726eaaee4db336
WIN-571EQ52NCR8\traininguser <taiga.ito@skywill.jp>
2026-04-23 10:56:16 +0900

    vsからのpush

--- 基礎2/演習5/演習5_3/MyService.java
+++ 基礎2/演習5/演習5_3/MyService.java
@@ -3,7 +3,7 @@ package 基礎2.演習5.演習5_3;
 public class MyService extends SecureService{
     @Override
     protected void execute(){
-        System.out.println("実際の処理内容");
+        System.out.println("実際の処理の内容");
 
     }
 
